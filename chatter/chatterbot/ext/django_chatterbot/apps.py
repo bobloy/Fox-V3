17c68f04da1d5982f5c80c4d504347839bb0dab9
bobloy <alboblexloy@gmail.com>
2018-03-06 15:36:44 -0500

    No chatterbot folder

--- chatter/chatterbot/ext/django_chatterbot/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoChatterBotConfig(AppConfig):
-
-    name = 'chatterbot.ext.django_chatterbot'
-    label = 'django_chatterbot'
-    verbose_name = 'Django ChatterBot'
