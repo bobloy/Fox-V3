b5927ddc5a5744970c173e60ff70f27b17acd40e
bobloy <alboblexloy@gmail.com>
2018-03-22 15:51:16 -0400

    defaults

--- werewolf/Roles/Villager.py
+++ werewolf/Roles/Villager.py
@@ -4,43 +4,8 @@ import discord
 
 from datetime import datetime,timedelta
 
-class Role:
-    """
-    Base Role class for werewolf game
-    
-    Category enrollment guide as follows (category property):
-        Town:
-        1: Random, 2: Investigative, 3: Protective, 4: Government,
-        5: Killing, 6: Power (Special night action)
-        
-        Werewolf:
-        11: Random, 12: Deception, 15: Killing, 16: Support
-        
-        Neutral:
-        21: Benign, 22: Evil, 23: Killing
-        
-        
-        Example category:
-        category = [1, 5, 6] Could be Veteran
-        category = [1, 5] Could be Bodyguard
-        category = [11, 16] Could be Werewolf Silencer
-        
-    
-    Action guide as follows (on_event function):
-        _at_night_start
-        0. No Action
-        1. Detain actions (Jailer/Kidnapper)
-        2. Group discussions and Pick targets
-        
-        _at_night_end
-        1. Self actions (Veteran)
-        2. Target switching and role blocks (bus driver, witch, escort)
-        3. Protection / Preempt actions (bodyguard/framer)
-        4. Non-disruptive actions (seer/silencer)
-        5. Disruptive actions (werewolf kill)
-        6. Role altering actions (Cult / Mason)
-    """
-    
+class Villager(Role):
+     
     rand_choice = False  # Determines if it can be picked as a random role (False for unusually disruptive roles)
     category = [0]      # List of enrolled categories (listed above)
     allignment = 0      # 1: Town, 2: Werewolf, 3: Neutral
