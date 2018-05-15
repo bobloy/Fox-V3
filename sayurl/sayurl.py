6272edc2611f789e7fc714dcc75c5ac2ce001699
Bobloy <alboblexloy@gmail.com>
2018-05-15 09:43:04 -0400

    For commands somehow

--- sayurl/sayurl.py
+++ sayurl/sayurl.py
@@ -1,7 +1,7 @@
 import aiohttp
 import html2text
 
-from redbot.core import Config
+from redbot.core import Config, commands
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import pagify
 
