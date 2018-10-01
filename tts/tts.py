ae4fbbcc58bff9a1272a5df2b4628592435e5d39
bobloy <alboblexloy@gmail.com>
2018-10-01 10:17:28 -0400

    Additional list and tts descriptions

--- tts/tts.py
+++ tts/tts.py
@@ -8,7 +8,7 @@ from redbot.core.bot import Red
 
 class TTS:
     """
-    V3 Cog Template
+    Send Text-to-Speech messages
     """
 
     def __init__(self, bot: Red):
@@ -24,9 +24,7 @@ class TTS:
     @commands.command(aliases=["t2s", "text2"])
     async def tts(self, ctx: commands.Context, *, text: str):
         """
-       My custom cog
-       
-       Extra information goes here
+       Send Text to speech messages as an mp3
        """
         mp3_fp = io.BytesIO()
         tts = gTTS(text, 'en')
