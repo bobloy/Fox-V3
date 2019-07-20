f8f1d18a598326c749c0f05efdcb5c3f9486682c
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-07-20 03:28:55 +0100

    Update qrinvite.py

--- qrinvite/qrinvite.py
+++ qrinvite/qrinvite.py
@@ -60,7 +60,8 @@ class QRInvite(Cog):
             )
             return
 
-        extension = pathlib.Path(image_url).parts[-1].replace(".", "?").split("?")[1]
+        e = pathlib.Path(image_url).parts[-1].replace(".", "?").split("?")[1]
+        extention = str(e)
 
         path: pathlib.Path = cog_data_path(self)
         image_path = path / (ctx.guild.icon + "." + extension)
