96f77ca6f234802c902041046f675a17421c80e6
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-02 17:47:43 +0100

    Depreaction warning

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -4,6 +4,6 @@ from redbot.core import data_manager
 
 def setup(bot):
     n = Hangman(bot)
-    data_manager.load_bundled_data(n, __file__)
+    data_manager.bundled_data_path(n)
     bot.add_cog(n)
     bot.add_listener(n.on_react, "on_reaction_add")
