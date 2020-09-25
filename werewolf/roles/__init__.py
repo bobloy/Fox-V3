a0c645bd28f9aa07029776e7b1c99baae4f6919d
bobloy <alboblexloy@gmail.com>
2020-09-25 15:56:33 -0400

    Precarious import order

--- werewolf/roles/__init__.py
+++ werewolf/roles/__init__.py
@@ -1,6 +1,11 @@
-from .seer import Seer
-from .shifter import Shifter
-from .vanillawerewolf import VanillaWerewolf
 from .villager import Villager
+from .seer import Seer
+
+from .vanillawerewolf import VanillaWerewolf
+
+from .shifter import Shifter
+
+# Don't sort these imports. They are unstably in order
+# TODO: Replace with unique IDs for roles in the future
 
 __all__ = ["Seer", "Shifter", "VanillaWerewolf", "Villager"]
