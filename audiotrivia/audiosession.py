3fceea634bb6a9e6e590a013cda8819d653f1532
bobloy <alboblexloy@gmail.com>
2020-10-01 09:10:02 -0400

    Audiotrivia updates from lessons learned attempting core

--- audiotrivia/audiosession.py
+++ audiotrivia/audiosession.py
@@ -2,9 +2,8 @@
 import asyncio
 import logging
 
-import lavalink
-from lavalink.enums import LoadType
 from redbot.cogs.trivia import TriviaSession
+from redbot.cogs.trivia.session import _parse_answers
 from redbot.core.utils.chat_formatting import bold
 
 log = logging.getLogger("red.fox_v3.audiotrivia.audiosession")
@@ -13,14 +12,14 @@ log = logging.getLogger("red.fox_v3.audiotrivia.audiosession")
 class AudioSession(TriviaSession):
     """Class to run a session of audio trivia"""
 
-    def __init__(self, ctx, question_list: dict, settings: dict, player: lavalink.Player):
+    def __init__(self, ctx, question_list: dict, settings: dict, audio = None):
         super().__init__(ctx, question_list, settings)
 
-        self.player = player
+        self.audio = audio
 
     @classmethod
-    def start(cls, ctx, question_list, settings, player: lavalink.Player = None):
-        session = cls(ctx, question_list, settings, player)
+    def start(cls, ctx, question_list, settings, audio = None):
+        session = cls(ctx, question_list, settings, audio)
         loop = ctx.bot.loop
         session._task = loop.create_task(session.run())
         return session
@@ -34,57 +33,89 @@ class AudioSession(TriviaSession):
         await self._send_startup_msg()
         max_score = self.settings["max_score"]
         delay = self.settings["delay"]
+        audio_delay = self.settings["audio_delay"]
         timeout = self.settings["timeout"]
-        for question, answers in self._iter_questions():
+        if self.audio is not None:
+            import lavalink
+
+            player = lavalink.get_player(self.ctx.guild.id)
+            player.store("channel", self.ctx.channel.id)  # What's this for? I dunno
+            await self.audio.set_player_settings(self.ctx)
+        else:
+            lavalink = None
+            player = False
+
+        for question, answers, audio_url in self._iter_questions():
             async with self.ctx.typing():
                 await asyncio.sleep(3)
             self.count += 1
-            await self.player.stop()
+            msg = bold(f"Question number {self.count}!") + f"\n\n{question}"
+            if player:
+                await player.stop()
+            if audio_url:
+                if not player:
+                    log.debug("Got an audio question in a non-audio trivia session")
+                    continue
 
-            msg = bold(f"Question number {self.count}!") + "\n\nName this audio!"
+                load_result = await player.load_tracks(audio_url)
+                if (
+                        load_result.has_error
+                        or load_result.load_type != lavalink.enums.LoadType.TRACK_LOADED
+                ):
+                    await self.ctx.maybe_send_embed(
+                        "Audio Track has an error, skipping. See logs for details"
+                    )
+                    log.info(f"Track has error: {load_result.exception_message}")
+                    continue
+                tracks = load_result.tracks
+                track = tracks[0]
+                seconds = track.length / 1000
+                track.uri = ""  # Hide the info from `now`
+                if self.settings["repeat"] and seconds < audio_delay:
+                    # Append it until it's longer than the delay
+                    tot_length = seconds + 0
+                    while tot_length < audio_delay:
+                        player.add(self.ctx.author, track)
+                        tot_length += seconds
+                else:
+                    player.add(self.ctx.author, track)
+
+                if not player.current:
+                    await player.play()
             await self.ctx.maybe_send_embed(msg)
             log.debug(f"Audio question: {question}")
-            # print("Audio question: {}".format(question))
 
-            # await self.ctx.invoke(self.audio.play(ctx=self.ctx, query=question))
-            # ctx_copy = copy(self.ctx)
-
-            # await self.ctx.invoke(self.player.play, query=question)
-            query = question.strip("<>")
-            load_result = await self.player.load_tracks(query)
-            log.debug(f"{load_result.load_type=}")
-            if load_result.has_error or load_result.load_type != LoadType.TRACK_LOADED:
-                await self.ctx.maybe_send_embed(f"Track has error, skipping. See logs for details")
-                log.info(f"Track has error: {load_result.exception_message}")
-                continue  # Skip tracks with error
-            tracks = load_result.tracks
-
-            track = tracks[0]
-            seconds = track.length / 1000
-
-            if self.settings["repeat"] and seconds < delay:
-                # Append it until it's longer than the delay
-                tot_length = seconds + 0
-                while tot_length < delay:
-                    self.player.add(self.ctx.author, track)
-                    tot_length += seconds
-            else:
-                self.player.add(self.ctx.author, track)
-
-            if not self.player.current:
-                log.debug("Pressing play")
-                await self.player.play()
-
-            continue_ = await self.wait_for_answer(answers, delay, timeout)
+            continue_ = await self.wait_for_answer(
+                answers, audio_delay if audio_url else delay, timeout
+            )
             if continue_ is False:
                 break
             if any(score >= max_score for score in self.scores.values()):
                 await self.end_game()
                 break
         else:
-            await self.ctx.send("There are no more questions!")
+            await self.ctx.maybe_send_embed("There are no more questions!")
             await self.end_game()
 
     async def end_game(self):
         await super().end_game()
-        await self.player.disconnect()
+        if self.audio is not None:
+            await self.ctx.invoke(self.audio.command_disconnect)
+
+    def _iter_questions(self):
+        """Iterate over questions and answers for this session.
+
+        Yields
+        ------
+        `tuple`
+            A tuple containing the question (`str`) and the answers (`tuple` of
+            `str`).
+
+        """
+        for question, q_data in self.question_list:
+            answers = _parse_answers(q_data["answers"])
+            _audio = q_data["audio"]
+            if _audio:
+                yield _audio, answers, question.strip("<>")
+            else:
+                yield question, answers, _audio
